b01eb5677f4a80f3a1c365af88e8d53694e12e08
norr0065 <norr0065@algonquinlive.com>
2015-03-19 10:04:39 -0400

    Styling Lightboxes

--- js/main.js
+++ js/main.js
@@ -17,4 +17,21 @@ $tabs.on('click', 'a', function (e) {
 
   $tabs.find('.js-current').removeClass('js-current');
   $(this).addClass('js-current');
+    
+});
+
+var $thumbs =$('.thumbnails');
+var $lbImg = $('.lightbox-img');
+var $lb = $ ('.lightbox');
+var $btnClose = $('.btn-close');
+
+$thumbs.on('click', 'a', function (e) {
+    e.preventDefault();
+var big = $(this).attr('href');
+  $lbImg.attr('src', big);
+  $lb.attr('data-state','visible');
+});
+
+$btnClose.on('click', function () {
+    $lb.attr('data-state','hidden');
 });
