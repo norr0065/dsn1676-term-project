600e68f5a670595814e02740e2ac0576a4647a63
norr0065 <norr0065@algonquinlive.com>
2015-03-18 23:38:18 -0400

    Added Javascript Elements

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
-
 var $navBtn = $('.nav-btn');
 var $navagationDropdown = $('.navagation-dropdown');
 
-$navBtn.on('click', function () {
+$navBt.on('click', function () {
     $navagationDropdown.toggleClass('js-nav-open');
+});
+
+var $tabs = $('.tabs');
+var $panels = $('.panel');
+
+$tabs.on('click', 'a', function (e) {
+    e.preventDefault();
+    var id = $(this).attr('href');
+    
+$panels.filter(':not([hidden])').attr('hidden', true);
+    $(id).removeAttr('hidden')
+    
+    $tabs.find('.js-current').removeClass('js-current')
 });
